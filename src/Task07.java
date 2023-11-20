3a2f448ed42c9e52b507f8588a6e707c64db4afa
TaronJar <taron64arm@gmail.com>
2023-11-20 20:42:58 +0400

    add commit Task07.java

---
+++ src/Task07.java
@@ -0,0 +1,11 @@
+public class Task07 {
+    public static void main(String[] args) {
+      System.out.println("Задание: 7");
+int num = 5;
+for (int i = 31; i >= 0; i--) {
+    int bit = (num >> i) & 1;
+    System.out.println(bit);
+}
+
+    }
+}
